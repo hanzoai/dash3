873447bf4fb0e062ff3e689cedb5004a6f6758a0
David Tai <david@verus.io>
2020-02-17 12:15:55 -0600

    add support for projected revenue number

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,4 @@
-export let HANZO_ENDPOINT       = 'https://api.hanzo.ai'
-export let STRIPE_CLIENT_ID     = 'ca_53yyPzxlPsdAtzMEIuS2mXYDp4FFXLmm'
-export let STRIPE_REDIRECT_URI  = 'https://api.hanzo.ai/stripe/callback'
+// export const HANZO_ENDPOINT       = 'https://api.hanzo.ai'
+export const HANZO_ENDPOINT = 'https://api-dot-hanzo-staging-249116.appspot.com'
+export const STRIPE_CLIENT_ID = 'ca_53yyPzxlPsdAtzMEIuS2mXYDp4FFXLmm'
+export const STRIPE_REDIRECT_URI = `${HANZO_ENDPOINT}/stripe/callback`
