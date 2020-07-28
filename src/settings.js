c29eac7ebb7475f6213f2bd5a43d6738c7c73813
David Tai <david@verus.io>
2020-07-28 09:39:51 -0500

    update stripe client key

--- src/settings.js
+++ src/settings.js
@@ -26,5 +26,5 @@ try {
 
 export const HANZO_ENDPOINT = 'https://api.hanzo.ai'
 // export const HANZO_ENDPOINT = 'https://api-dot-hanzo-staging-249116.appspot.com'
-export const STRIPE_CLIENT_ID = 'ca_53yyPzxlPsdAtzMEIuS2mXYDp4FFXLmm'
+export const STRIPE_CLIENT_ID = 'ca_53yyRUNpMtTRUgMlVlLAM3vllY1AVybU'
 export const STRIPE_REDIRECT_URI = `${HANZO_ENDPOINT}/stripe/callback`
